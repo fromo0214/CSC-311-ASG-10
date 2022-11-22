ba8dd7a9ab514f43dc5ffeaaf9b7e08572b5ef56
Fernando Romo <faromo0214@gmail.com>
2022-11-22 12:12:12 -0800

    Main

--- src/csudh/csc311/asg10/MyTreeMap.java
+++ src/csudh/csc311/asg10/MyTreeMap.java
@@ -11,6 +11,8 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 
+import javax.lang.model.util.ElementScanner14;
+
 /**
  * Implementation of a Map using a binary search tree.
  *
@@ -72,6 +74,24 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 		Comparable<? super K> k = (Comparable<? super K>) target;
 
 		// TODO: FILL THIS IN!
+		Node node = root;
+
+		while(node != null)
+		{
+			int cmp = k.compareTo(node.key);
+			if(cmp < 0)
+			{
+				node = node.left;
+			}
+			else if (cmp > 0)
+			{
+				node = node.right;
+			}
+			else
+			{
+				return node;
+			}
+		}
 		return null;
 	}
 
@@ -96,6 +116,22 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 
 	private boolean containsValueHelper(Node node, Object target) {
 		// TODO: FILL THIS IN!
+		if(node == null)
+		{
+			return false;
+		}
+		if(equals(target, node.value))
+		{
+			return true;
+		}
+		if(containsValueHelper(node.left, target))
+		{
+			return true;
+		}
+		if(containsValueHelper(node.right, target))
+		{
+			return true;
+		}
 		return false;
 	}
 
@@ -122,7 +158,20 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 	public Set<K> keySet() {
 		Set<K> set = new LinkedHashSet<K>();
 		// TODO: FILL THIS IN!
+		addInOrder(root, set);
 		return set;
+	}
+
+	private void addInOrder(Node node, Set<K> set) 
+	{
+		if(node == null)
+		{
+			return;
+		}
+		addInOrder(node.left, set);
+		set.add(node.key);
+		addInOrder(node.right, set);
+
 	}
 
 	@Override
@@ -140,7 +189,39 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 
 	private V putHelper(Node node, K key, V value) {
 		// TODO: FILL THIS IN!
-		return null;
+		Comparable<? super K> k = (Comparable<? super K>) key;
+		int cmp = k.compareTo(node.key);
+
+		if(cmp < 0)
+		{
+			if(node.left == null)
+			{
+				node.left = new Node(key, value);
+				size++;
+				return null;
+			}
+			else
+			{
+				return putHelper(node.left, key, value);
+			}
+		}
+
+		if(cmp > 0)
+		{
+			if(node.right == null)
+			{
+				node.right = new Node(key, value);
+				size++;
+				return null;
+			}
+			else
+			{
+				return putHelper(node.right, key, value);
+			}
+		}
+		V oldValue = node.value;
+		node.value = value;
+		return oldValue;
 	}
 
 	@Override
